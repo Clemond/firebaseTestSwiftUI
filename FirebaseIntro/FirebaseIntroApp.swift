ef0d196710d9c662a0de2c8028448724950b0e27
Clemond <quackshows11@gmail.com>
2024-09-30 09:14:14 +0200

    Initial Commit

---
+++ FirebaseIntro/FirebaseIntroApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseIntroApp.swift
+//  FirebaseIntro
+//
+//  Created by Nicholas Nieminen Jönsson on 2024-09-30.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseIntroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FirebaseIntroTests/FirebaseIntroTests.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseIntroTests.swift
+//  FirebaseIntroTests
+//
+//  Created by Nicholas Nieminen Jönsson on 2024-09-30.
+//
+
+import Testing
+@testable import FirebaseIntro
+
+struct FirebaseIntroTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
